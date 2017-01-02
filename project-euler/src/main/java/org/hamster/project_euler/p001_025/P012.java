ab80348506052be6fbd89d7dff45db8bae16b8c0
Jack Yin <grossopaforever@gmail.com>
2017-01-02 12:31:59 +0800

    P012 WIP

---
+++ project-euler/src/main/java/org/hamster/project_euler/p001_025/P012.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.hamster.project_euler.p001_025;
+
+import org.hamster.project_euler.base.Solution;
+
+/**
+ * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
+ * @since 1.0
+ */
+public class P012 implements Solution {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.hamster.project_euler.base.Solution#solution(double)
+     */
+    @Override
+    public Number solution(double input) {
+        return null;
+    }
+
+}
---
+++ project-euler/src/test/java/org/hamster/project_euler/test/p001_025/P012Test.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.hamster.project_euler.test.p001_025;
+
+import org.hamster.project_euler.p001_025.P012;
+import org.hamster.project_euler.test.base.AbstractSolutionTest;
+import org.hamster.project_euler.test.base.TestResult;
+
+/**
+ * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
+ * @since 1.0
+ */
+public class P012Test extends AbstractSolutionTest<P012> {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#example()
+     */
+    @Override
+    protected TestResult example() {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#solution()
+     */
+    @Override
+    protected TestResult solution() {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#problemClass()
+     */
+    @Override
+    protected Class<P012> problemClass() {
+        return P012.class;
+    }
+
+}
