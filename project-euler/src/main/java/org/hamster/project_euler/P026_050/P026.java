03d9c5ccbbe21c07d90f5a12d894d40cec002686
Jack Yin <grossopaforever@gmail.com>
2017-01-07 15:33:20 +0800

    P026

---
+++ project-euler/src/main/java/org/hamster/project_euler/P026_050/P026.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.hamster.project_euler.P026_050;
+
+import org.hamster.project_euler.base.Solution;
+
+/**
+ * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
+ * @version 1.0
+ */
+public class P026 implements Solution {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamster.project_euler.base.Solution#solution(double)
+     */
+    @Override
+    public Number solution(double input) {
+
+        return null;
+    }
+
+}
