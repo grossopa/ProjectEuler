6d3241efa7bc52934adbe704262ad4f7badb5702
Jack Yin <grossopaforever@gmail.com>
2017-01-07 15:16:27 +0800

    P025

---
+++ project-euler/src/main/java/org/hamster/project_euler/p001_025/P025.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.hamster.project_euler.p001_025;
+
+import org.hamster.project_euler.base.Solution;
+
+/**
+ * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
+ * @version 1.0
+ */
+public class P025 implements Solution {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamster.project_euler.base.Solution#solution(double)
+     */
+    @Override
+    public Number solution(double input) {
+        double max = 10;
+
+        double result = 0;
+        double current = 0;
+        double prev2 = 1;
+        double prev1 = 0;
+
+        int bufferredDigits = 1;
+
+        while (bufferredDigits < input) {
+            current = prev1 + prev2;
+            result++;
+            while (current >= max) {
+                current /= 10;
+                prev1 /= 10;
+                prev2 /= 10;
+                bufferredDigits++;
+            }
+            prev2 = prev1;
+            prev1 = current;
+
+        }
+
+        return result;
+    }
+
+}
---
+++ project-euler/src/test/java/org/hamster/project_euler/test/p001_025/P025Test.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.hamster.project_euler.test.p001_025;
+
+import org.hamster.project_euler.p001_025.P025;
+import org.hamster.project_euler.test.base.AbstractSolutionTest;
+import org.hamster.project_euler.test.base.TestResult;
+
+/**
+ * @author <a href="mailto:grossopaforever@gmail.com">Jack Yin</a>
+ * @version 1.0
+ */
+public class P025Test extends AbstractSolutionTest<P025> {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#example()
+     */
+    @Override
+    protected TestResult example() {
+        return TestResult.create(3, 12);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#solution()
+     */
+    @Override
+    protected TestResult solution() {
+        return TestResult.create(1000, 4782);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.hamster.project_euler.test.base.AbstractSolutionTest#problemClass()
+     */
+    @Override
+    protected Class<P025> problemClass() {
+        return P025.class;
+    }
+
+}
